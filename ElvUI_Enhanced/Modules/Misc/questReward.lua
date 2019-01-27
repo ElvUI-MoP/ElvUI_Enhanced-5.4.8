9134b9103aa280bc9a086e4f9f128affb7e23181
dethx <1835067+dethx@users.noreply.github.com>
2019-01-27 17:16:09 +0200

    updated questReward

--- ElvUI_Enhanced/Modules/Misc/questReward.lua
+++ ElvUI_Enhanced/Modules/Misc/questReward.lua
@@ -6,31 +6,36 @@ local format = string.format
 
 local function SelectQuestReward(index)
 	local btn = _G[("QuestInfoItem%d"):format(index)]
-	if(btn.type == "choice") then
-		QuestInfoItemHighlight:ClearAllPoints()
-		QuestInfoItemHighlight:SetAllPoints(btn)
-		QuestInfoItemHighlight:Show()
+
+	if btn.type == "choice" then
+		if E.private.skins.blizzard.enable and E.private.skins.blizzard.quest then
+			_G[btn:GetName()]:SetBackdropBorderColor(1, 0.80, 0.10)
+			_G[btn:GetName()].backdrop:SetBackdropBorderColor(1, 0.80, 0.10)
+			_G[btn:GetName().."Name"]:SetTextColor(1, 0.80, 0.10)
+		else
+			QuestInfoItemHighlight:ClearAllPoints()
+			QuestInfoItemHighlight:SetAllPoints(btn)
+			QuestInfoItemHighlight:Show()
+		end
 
 		QuestInfoFrame.itemChoice = btn:GetID()
 	end
 end
 
 function M:QUEST_COMPLETE()
-	if not E.private.general.selectQuestReward then return end
+	if not E.db.enhanced.general.selectQuestReward then return end
 
 	local choice, price = 1, 0
 	local num = GetNumQuestChoices()
 
-	if num <= 0 then
-		return
-	end
+	if num <= 0 then return end
 
 	for index = 1, num do
 		local link = GetQuestItemLink("choice", index)
-		if (link) then
-			local vsp = select(11, GetItemInfo(link))
-			if vsp and vsp > price then
-				price = vsp
+		if link then
+			local itemPrice = select(11, GetItemInfo(link))
+			if itemPrice and itemPrice > price then
+				price = itemPrice
 				choice = index
 			end
 		end
